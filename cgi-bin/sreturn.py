c5ee253c6a6ec576dad88afbe920762b84cef9f9
root <root@server.yadav.com>
2017-06-10 12:37:25 +0530

    Initial project..

---
+++ cgi-bin/sreturn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+print "content-type: text/html"
+print
+
+import cgi,commands,os
+data= cgi.FormContent()
+
+#print data
+data['OS']
+data1=data['OS']
+data2=data1[0]
+print data2
